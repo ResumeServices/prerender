8d517c6d06a575d9d7c84e2761df7bfe2f613f01
diegojonio <diegojonio@gmail.com>
2017-09-26 12:39:56 -0700

    Auto-scaling config - auto deploy with AWS CodeDeploy and CodePipeline

---
+++ deploy/pm2-deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu
+
+cd /home/ubuntu/prerender
+
+sudo n 6.10.2
+npm install
+echo "App installed"
+
+
+cat <<EOF > /home/ubuntu/prerender-pm2-config.json
+{
+  "apps":
+  [
+    {
+      "name": "prerender",
+      "cwd":"/home/ubuntu/prerender",
+      "script": "server.js",
+      "env": {
+        "PORT":"6000",
+        "NODE_ENV":"prod",
+        "ROOT_URL":"http://localhost:6000",
+        "IGNORE_QUERY_PARAMS":"utm_content,utm_term,utm_source,email_hash,utm_campaign,utm_medium,passwordinput,userid,code,src,imt,_ke",
+        "S3_BUCKET_NAME":"resume-prerender-cache",
+        "ALLOWED_DOMAINS":"www.resume.com,employer.resume.com",
+        "WAIT_AFTER_LAST_REQUEST":500,
+        "JS_CHECK_TIMEOUT":1000,
+        "EVALUATE_JAVASCRIPT_CHECK_TIMEOUT":1000,
+        "PAGE_DONE_CHECK_TIMEOUT":1000,
+        "MONGO_URL":"mongodb://prerender:n7ejAwRhkWhR@candidate.40.mongolayer.com:10700,candidate.41.mongolayer.com:10849/prerender?replicaSet=set-55b2caa535fae993ff0009a6",
+        "CACHE_TIME_TO_LIVE_IN_DAYS":4
+      }
+    }
+  ]
+}
+EOF
+
+
+sudo pm2 restart /home/ubuntu/prerender-pm2-config.json
+echo "App started"
+sudo pm2 save
+
+
