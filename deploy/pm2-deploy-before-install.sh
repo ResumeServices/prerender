546bd267441cfeff6d5e7414f30e17b09e231ca6
Diego Lins <diegojonio@gmail.com>
2018-08-07 11:53:30 -0700

    Tranfering back scripts from before install to launch script

--- deploy/pm2-deploy-before-install.sh
+++ deploy/pm2-deploy-before-install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-set -e
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu
-
-cd /tmp
-
-
-wget https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
-sudo chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent start
-
-sudo npm install -g n
 
 
